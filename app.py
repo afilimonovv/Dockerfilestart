7891ff637aae20b7bdce69b002f706085778f07f
afilimonovv <105278832+afilimonovv@users.noreply.github.com>
2022-05-16 10:40:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def hello():
     return "Hello DevOps!"
     if __name__ =='__main__':
         #Bind to Port if defined, otherwise default to 5000
-        #port=int(os.environ.get( 'PORT', 5000 ))
-        #app.run(host='192.168.0.107', port=port)
-        app.run()
+        port=int(os.environ.get( 'PORT', 5000 ))
+        app.run(host='192.168.0.107', port=port)
+        #app.run()
